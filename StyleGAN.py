8f5ddc991d481311c619580904e445077327af70
sam <sbbaumo@yahoo.com>
2021-03-30 22:51:03 -0500

    it technically works
    god it needs a refactor

--- StyleGAN.py
+++ StyleGAN.py
@@ -9,6 +9,7 @@ from scipy.stats import truncnorm
 
 # IMPORTANT CONSTANTS
 batch_size = 24
+num_images_fade_in=(800 * 1000)
 
 # LOADING DATA
 intel_image_transformation = transforms.Compose([
@@ -35,13 +36,36 @@ def display_image(images, num_display = 9, save_to_disk=False, save_dir='./outpu
     
     if save_to_disk:
         plt.savefig('{0}/{1}.png'.format(save_dir, filename))
-        return
+    else:
+        plt.show()
     
-    plt.show()
 
 def get_truncated_noise(n_samples, z_dim, truncation):
     truncated_noise = truncnorm.rvs(-truncation, truncation, size=(n_samples, z_dim ))
     return torch.Tensor(truncated_noise)
+
+
+def calculate_alphas(discriminator_count, num_images_fade_in):
+
+    alphas = [0 for x in range(6)] # Change range(x) to match number of alphas (# Blocks = # Alphas)
+
+    running_count = float(discriminator_count / num_images_fade_in) + 2
+    for index, val in enumerate(alphas):
+        
+        if running_count < 1.0:
+            continue
+        elif running_count >= 1.0 and running_count <= 2.0:
+            alphas[index] = round(running_count - 1, 7)
+        elif running_count > 2.0:
+            alphas[index] = None
+            if running_count < 3.0:
+                alphas[index] = 1
+        
+        running_count = running_count - 2
+    
+    if alphas[:1:-1][0] is None:
+        alphas[-1] = 1
+    return alphas
 
 
 class MappingLayers(nn.Module):
@@ -98,7 +122,7 @@ class StyleGANBlock(nn.Module):
     def __init__(self, in_channels, out_channels, image_color_channels=3, noise_dim=512, image_size=(8,8), previous_image_size=(4,4), kernel_size=3):
         super().__init__()
 
-        self.simple_upsample = nn.Upsample(image_size, mode='bilinear') # TODO, wrap in one nn.sequential?
+        self.simple_upsample = nn.Upsample(image_size, mode='bilinear')
 
         self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=1)
         self.inject_noise_1 = InjectSecondaryNoise(out_channels)
@@ -153,15 +177,13 @@ class StyleGANBlock(nn.Module):
             raise ValueError('alpha not valid!')
 
 class StyleGAN(nn.Module):
-    def __init__(self, z_size=512, image_channels=3, num_images_fade_in=(800 * 1000)):
+    def __init__(self, z_size=512, image_channels=3):
         super().__init__()
         
         # Save parameters.
         self.z_size = z_size
 
         self.image_channels = image_channels
-
-        self.num_images_fade_in = num_images_fade_in
 
         # Z -> W
         self.noise_mapping = MappingLayers(z_size)
@@ -191,9 +213,9 @@ class StyleGAN(nn.Module):
 
         # Number of alphas: 6
     
-    def forward(self, z_noise, discriminator_count):
+    def forward(self, z_noise, discriminator_count, num_images_fade_in):
         # calculate alphas
-        alphas = self.calculate_alphas(discriminator_count)
+        alphas = calculate_alphas(discriminator_count, num_images_fade_in)
 
         # forward pass
         w_noise = self.noise_mapping(z_noise)
@@ -223,32 +245,131 @@ class StyleGAN(nn.Module):
 
         return output
 
-    def calculate_alphas(self, discriminator_count):
-
-        alphas = [0 for x in range(6)] # Change this number to match number of alphas
-
-        running_count = float(discriminator_count / self.num_images_fade_in) + 2
-        for index, val in enumerate(alphas):
-            
-            if running_count < 1.0:
-                continue
-            elif running_count >= 1.0 and running_count <= 2.0:
-                alphas[index] = round(running_count - 1, 7)
-            elif running_count > 2.0:
-                alphas[index] = None
-                if running_count < 3.0:
-                    alphas[index] = 1
-            
-            running_count = running_count - 2
-        
-        if alphas[:1:-1][0] is None:
-            alphas[-1] = 1
-        return alphas
 
 
 class Critic(nn.Module):
-    def __init__(self):
+    def __init__(self, image_channels=3):
         super().__init__()
 
-for x, _ in tqdm(images):
-    print()
+        # 128 -> 64
+        self.block_1 = CriticBlock(in_size=128, out_size=256)
+
+        # 64 -> 32
+        self.block_2 = CriticBlock(in_size=256)
+
+        # 32 -> 16
+        self.block_3 = CriticBlock()
+
+        # 16 -> 8
+        self.block_4 = CriticBlock()
+
+        # 8 -> 4
+        self.block_5 = CriticBlock()
+
+        # Final Layers once you've just downsampled to 4x4
+
+        self.final_layers = nn.Sequential(
+            MiniBatchStdDev(),
+            nn.Conv2d(513, 512, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, kernel_size=4),
+            nn.LeakyReLU(0.2),
+            nn.Flatten(end_dim=-1),
+            nn.Linear(512, 1)
+        )
+
+        self.from_rgb = nn.Conv2d(image_channels, 512, kernel_size=1)
+    
+    def forward(self, x, discriminator_count, num_images_fade_in):
+        alphas = calculate_alphas(discriminator_count, num_images_fade_in)[::-1]
+
+        out = x
+
+        if alphas[0] != 0:
+            out = self.block_1(out, alpha=alphas[0])
+        
+        if alphas[1] != 0:
+            out = self.block_2(out, alpha=alphas[1])
+
+        if alphas[2] != 0:
+            out = self.block_3(out, alpha=alphas[2])
+        
+        if alphas[3] != 0:
+            out = self.block_4(out, alpha=alphas[3])
+        
+        if alphas[4] != 0:
+            out = self.block_5(out, alpha=alphas[4])
+        elif alphas[4] == 0:
+            out = self.from_rgb(out)
+
+        return self.final_layers(out)
+
+        
+
+class MiniBatchStdDev(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x):
+        if x.dim() != 4:
+            raise ValueError('Tensor dim must be 4, got {} instead.'.format(x.dim()))
+        
+        output = torch.cat((x, torch.std(x[:, None], dim=2)), 1)
+
+        return torch.cat((x, torch.std(x[:, None], dim=2)), 1)
+
+class CriticBlock(nn.Module):
+    def __init__(self, image_channels=3, in_size=512, out_size=512, leaky_relu_negative_slope=0.2):
+        super().__init__()
+
+        self.from_rgb = nn.Conv2d(image_channels, in_size, 1)
+
+        self.main_block = nn.Sequential(
+            nn.Conv2d(in_size, out_size, 3, padding=1),
+            nn.LeakyReLU(leaky_relu_negative_slope),
+            nn.Conv2d(out_size, out_size, 3, padding=1),
+            nn.LeakyReLU(leaky_relu_negative_slope),
+        )
+
+        self.downsample = nn.AvgPool2d(2)
+    
+    def forward(self, x, alpha=None):
+        if alpha is None:
+            out = self.from_rgb(x)
+            return self.downsample(self.main_block(out))
+        elif alpha > 0:
+
+            conv_out = self.downsample(self.main_block(self.from_rgb(x)))
+
+            simple_downsample = self.from_rgb(self.downsample(x))
+
+            return torch.lerp(simple_downsample, conv_out, alpha)
+        elif alpha == 0:
+            raise ValueError("this layer should not be activated with an alpha of 0!")
+
+
+class Debug(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x):
+        print(x.shape)
+        return x
+
+def test():
+
+    a = get_truncated_noise(10, 512, 1)
+    gen = StyleGAN()
+
+    critic = Critic()
+    out = gen.forward(a, 5, 20)
+
+    b = critic.forward(out, 5, 20)
+
+    print(b)
+
+# for x, _ in tqdm(images):
+#     print()
+
+test()
+
--- test.py
+++ test.py
@@ -3,27 +3,39 @@ import torchvision
 from torchvision import datasets, transforms
 from torch import nn
 
-def calculate_alphas(num_images_fade_in, discriminator_count):
+# def calculate_alphas(num_images_fade_in, discriminator_count):
 
-    alphas = [0 for x in range(6)]
+#     alphas = [0 for x in range(6)] # Change this number to match number of alphas
 
-    running_count = float(discriminator_count / num_images_fade_in) + 2
-    for index, val in enumerate(alphas):
+#     running_count = float(discriminator_count / num_images_fade_in) + 2
+#     for index, val in enumerate(alphas):
         
-        if running_count < 1.0:
-            return alphas
-        elif running_count >= 1.0 and running_count <= 2.0:
-            alphas[index] = round(running_count - 1, 7)
-        elif running_count > 2.0:
-            alphas[index] = None
-            if running_count < 3.0:
-                alphas[index] = 1
+#         if running_count < 1.0:
+#             continue
+#         elif running_count >= 1.0 and running_count <= 2.0:
+#             alphas[index] = round(running_count - 1, 7)
+#         elif running_count > 2.0:
+#             alphas[index] = None
+#             if running_count < 3.0:
+#                 alphas[index] = 1
         
-        running_count = running_count - 2
+#         running_count = running_count - 2
     
-        
+#     if alphas[:1:-1][0] is None:
+#         alphas[-1] = 1
+#     return alphas
 
-a = [0, 0, None]
-print(a[:1:-1][0] is None)
-a[-1] = 0
-print(a)
+
+# size = 1024
+# 
+
+# d = torch.randn((4, 512, 4, 4))
+# # print(torch.std(d[:, None], dim=2).shape)
+
+# e = torch.cat((d, torch.std(d[:, None], dim=2)), 1)
+# print(e.shape)
+
+a = torch.ones((4,4))
+b = torch.zeros((4,4))
+
+print(torch.lerp(b, a, .6))
